ad517abcdb9630a73d0caa4a2a4cb5893fd1200f
mastershifu430 <150873222+mastershifu430@users.noreply.github.com>
2023-12-13 01:17:32 -0500

    Create main.py
    first

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex3 import *
+
+y = home_data.SalePrice
+
+feature_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF", "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+
+# Select data corresponding to features in feature_names
+X = home_data[feature_names]
+
+from sklearn.tree import DecisionTreeRegressor
+#specify the model. 
+#For model reproducibility, set a numeric value for random_state when specifying the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit the model
+iowa_model.fit(X,y)
+
+predictions = iowa_model.predict(X)

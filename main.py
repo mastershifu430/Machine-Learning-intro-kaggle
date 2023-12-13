8a6b036738d426896640a8f0f878e171b07ae263
mastershifu430 <150873222+mastershifu430@users.noreply.github.com>
2023-12-13 08:59:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
-# Code you have previously used to load data
-import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-
-# Path of the file to read
-iowa_file_path = '../input/home-data-for-ml-course/train.csv'
-
-home_data = pd.read_csv(iowa_file_path)
-y = home_data.SalePrice
-feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = home_data[feature_columns]
-
-# Specify Model
-iowa_model = DecisionTreeRegressor()
-# Fit Model
-iowa_model.fit(X, y)
-
-print("First in-sample predictions:", iowa_model.predict(X.head()))
-print("Actual target values for those homes:", y.head().tolist())
-
-# Set up code checking
 from learntools.core import binder
 binder.bind(globals())
-from learntools.machine_learning.ex4 import *
-print("Setup Complete")
+from learntools.machine_learning.ex7 import *
 
-# Import the train_test_split function and uncomment
+# Set up filepaths
+import os
+if not os.path.exists("../input/train.csv"):
+    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")  
+    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv")
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
-# fill in and uncomment
+# Load the data, and separate the target
+iowa_file_path = '../input/train.csv'
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+
+# Create X (After completing the exercise, you can return to modify this line!)
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+
+# Select columns corresponding to features, and preview the data
+X = home_data[features]
+X.head()
+
+# Split into validation and training data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
 
-# Specify the model
-iowa_model = DecisionTreeRegressor(random_state = 1)
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
 
-# Fit iowa_model with the training data.
-iowa_model.fit(train_X, train_y)
+rf_model_on_full_data = RandomForestRegressor(random_state=1)
 
-# Predict with all validation observations
-val_predictions = iowa_model.predict(val_X)
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(X, y)
 
-from sklearn.metrics import mean_absolute_error
-val_mae = mean_absolute_error(val_y, val_predictions)
+test_data_path = '../input/test.csv'
 
-# uncomment following line to see the validation_mae
-print(val_mae)
+# read test data file using pandas
+test_data = pd.read_csv(test_data_path)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = test_data[features]
+
+# make predictions which we will submit. 
+test_preds = rf_model_on_full_data.predict(test_X)
+
+
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': test_preds})
+output.to_csv('submission.csv', index=False)
